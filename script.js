f6a1bf2a6e61b02a456402d03ea9f49ef6a39258
Hovo <mnoyan.hovhannes@mail.ru>
2022-09-29 16:05:39 +0400

    final commit

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 "use stricte";
-function genericFun (a,b,c) {
-switch (c) {
-    case "+":return a + b;
-     
-     case "*": return a * b;
-     
-     case "/": return a / b;
-     
-     case "-": return a - b;
-     default: return"invalid argument";
-}
+function genericFun (x,y,simble) {
+    switch (simble) {
+        case "+": return x + y;
+        case "*": return x * y;
+        case "/": return x / y;
+        case "-": return x - y;
+        default: return "invalid argument";
+    }
 
 }
-console.log(genericFun(2,3,'*'));
+
 
