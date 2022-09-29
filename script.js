1fab4399de16e821628d5ec696d19dc9e2208258
Hovo <mnoyan.hovhannes@mail.ru>
2022-09-29 16:23:26 +0400

    working functionality

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 "use stricte";
-function getResult (x, y ,mathSimbol) {
-    switch(mathSimbol) {
-        case "+": return x + y;
-        case "*": return x * y;
-        case "/": return x / y;
-        case "-": return x - y;
+function getResult(x, y ,mathSymbol) {
+    switch (mathSymbol) {
+        case"+": return x + y;
+        case"*": return x * y;
+        case"/": return x / y;
+        case"-": return x - y;
         default: return "invalid argument";
     }
 }
-
-
