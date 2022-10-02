0a894f254e331bbfeceb9cc8c9971871dc992059
Hovo <mnoyan.hovhannes@mail.ru>
2022-10-02 12:13:17 +0400

    for watching

--- script/script.js
+++ script/script.js
@@ -1,8 +1,32 @@
 "use stricte";
 
-let btns = document.querySelectorAll("button"),
-    valueInputOne = document.querySelector("#value_number_one"),
-    valueInputTwo = document.querySelector("#value_number_two");
+let btns = document.querySelectorAll(".btn"),
+    display = document.querySelector(".display"),
+    btnOnOff = document.querySelector("#btns"),
+    btnAc = document.querySelector(".btns"),
+    btn1 = document.querySelector(".btn1");
+    let arr = [];
+    btns.forEach((item) => {
+item.addEventListener("click", (e) => {
+display.innerHTML += e.target.value;
+for (let i = 0; i < display.textContent.length; i++) {
+    if (display.textContent[i] === "+" || display.textContent[i] === "-" || display.textContent[i] === "*" || display.textContent[i] === "/" ) {
+        
+arr[0] = display.textContent.substring(0, i);
+arr[1] = display.textContent.substring(i + 1, display.textContent.length );
+arr[2] = display.textContent.substring(i, i + 1);
+console.log(arr);
+    }
+}
+});
+    });
+    btnAc.addEventListener("click", () => {
+        display.innerHTML = "";
+        arr = [];
+    });
+    btnAc.addEventListener('click', () => {
+
+    });
 
 function getResult(x, y, mathSymbol) {
     switch (mathSymbol) {
@@ -14,8 +38,6 @@ function getResult(x, y, mathSymbol) {
     }
 }
 
-btns.forEach(function (item) {
-    item.addEventListener("click", (e) => {
-        alert(getResult (valueInputOne.value,valueInputTwo.value,e.target.value));
-  });
-});  
+btn1.addEventListener('click',() => {
+    display.innerHTML += ` = ${ getResult(+arr[0], +arr[1], arr[2])}`;
+});
