035e7dcb4c58eee475424dfd30f9c19c906786e3
Hovo <mnoyan.hovhannes@mail.ru>
2022-10-07 15:21:49 +0400

    the code is working

--- script/script.js
+++ script/script.js
@@ -2,44 +2,121 @@
 
 let btns = document.querySelectorAll(".btn"),
     display = document.querySelector(".display"),
-    btnOnOff = document.querySelector("#btns"),
+    btnOnOff = document.querySelector("#onOff"),
     btnAc = document.querySelector(".btns"),
     btn1 = document.querySelector(".btn1"),
     arr = [];
+let sum = 0;
+let newArryOne = [];
+let newArryTwo = [];
+let i = 0;
+let str = "";
+let boolianShafl = true;
+let buttonsBoolian = 1;
+function multiplication(x, y) {
+    return x * y;
+}
 
-    btns.forEach((item) => {
-        item.addEventListener("click", (e) => {
+function subtraction(x, y) {
+    return x / y;
+}
+function distinguiseSymbol(item) {
+    item.addEventListener("click", (e) => {
+        if (e.target.value !== "+" && e.target.value !== "-" && e.target.value !== "*" && e.target.value !== "/") {
+            str += e.target.value;
+            arr[i] = str;
             display.innerHTML += e.target.value;
-            for (let i = 0; i < display.textContent.length; i++) {
-            if (display.textContent[i] === "+" || display.textContent[i] === "-" || display.textContent[i] === "*" || display.textContent[i] === "/" ) {
-            arr[0] = display.textContent.substring(0, i);
-            arr[1] = display.textContent.substring(i + 1, display.textContent.length );
-            arr[2] = display.textContent.substring(i, i + 1);
-
-    }
-}
-});
+            boolianShafl = true;
+        } else if (boolianShafl) {
+            str = "";
+            arr[i + 1] = e.target.value;
+            i += 2;
+            display.innerHTML += e.target.value;
+            boolianShafl = false;
+        }
     });
-
-    btnAc.addEventListener("click", () => {
-        display.innerHTML = "";
+}
+function getMultiplicationSubtraction() {
+    for (let i = 0; i < arr.length ; i ++) {
+        if (arr[i] === "*" || arr[i] === "/") {
+            if(arr[i] === "*") {
+                sum =  multiplication(+arr[i - 1], +arr[i + 1]);
+                newArryTwo = arr.slice(0, i - 1);
+                newArryTwo.push(sum);
+                newArryOne = arr.slice(i + 2, );
+                arr = newArryTwo.concat(newArryOne);
+                i--;
+            } else {
+                sum = subtraction(arr[i - 1], arr[i + 1]);
+                newArryTwo = arr.slice(0, i - 1);
+                newArryTwo.push(sum);
+                newArryOne = arr.slice(i + 2, );
+                arr = newArryTwo.concat(newArryOne);
+                i--;
+            }
+        }             
+    }
+    return sum;
+}
+function showInDIspaly() {
+    display.innerHTML = finelResualt(arr, sum);
+    arr = [];
+    arr[0] = display.textContent;
+    i = 0;
+    boolianShafl = true;
+    str = "";
+}
+function clearDisplay() {
+    display.innerHTML = "";
+    arr = [];
+    i = 0;
+    str = "";
+}
+function onOff() {
+btns.forEach(function(item) {
+    item.disabled = buttonsBoolian;
+    if(buttonsBoolian === 1) {
+        buttonsBoolian = 0;
+    } else {
+        buttonsBoolian = 1;
         arr = [];
-    });
-    
-    btnAc.addEventListener('click', () => {
-
-    });
-
-function getResult(x, y, mathSymbol) {
-    switch (mathSymbol) {
-        case "+": return x + y;
-        case "*": return x * y;
-        case "/": return x / y;
-        case "-": return x - y;
-        default: return "invalid argument";
-    }
+        sum = 0;
+        newArryOne = [];
+        newArryTwo = [];
+        i = 0;
+        str = "";
+        boolianShafl = true;
+        buttonsBoolian = 1;
+        display.innerHTML = "";
+        }
+    }); 
 }
+function finelResualt(arr, sum) {
+    sum = +arr[0];
+    for(let i = 0; i < arr.length - 1 / 2; i += 2) {
+        switch(arr[i + 1]) {
+            case "+": sum += +arr[i + 2];
+            break;
+            case "-": sum -= +arr[i + 2];
+            break;
+        }
+    }
+    return sum;
+}
+onOff();
+
+btns.forEach((item) => {
+    distinguiseSymbol(item);
+});
 
 btn1.addEventListener('click',() => {
-    display.innerHTML += ` = ${ getResult(+arr[0], +arr[1], arr[2])}`;
+    getMultiplicationSubtraction();
+    showInDIspaly();
+});
+btnAc.addEventListener("click", () => {
+    clearDisplay();
+});
+
+btnOnOff.addEventListener('click', () => {
+    onOff();
 });
